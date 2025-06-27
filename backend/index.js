d619f91dcc5f202234613e801b4c0619a51000c5
Saurabh Kumar <kumarsaurabh08112003@gmail.com>
2025-06-27 18:52:11 +0530

    front end backend both updated

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 // const bodyParser = require("body-parser")
 const app = express()
-// const Routes = require("./routes/route.js")
+const Routes = require("./routes/route.js")
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 dotenv.config();
 
@@ -24,7 +24,7 @@ mongoose
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
-// app.use('/', Routes);
+app.use('/', Routes);
 
 app.listen(PORT, () => {
     console.log(`Server started at port no. ${PORT}`)
