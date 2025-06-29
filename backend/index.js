62f5fc4eaa586b7d6a06e592c6b467317e7e8186
Saurabh Kumar <kumarsaurabh08112003@gmail.com>
2025-06-29 23:32:48 +0530

    changes made

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,9 @@ dotenv.config();
 app.use(express.json({ limit: '10mb' }))
 app.use(cors())
 
-mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(console.log("Connected to MongoDB"))
+
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
 app.use('/', Routes);
